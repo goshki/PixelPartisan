b3988297f740f6925bd07ab8c41eafa2557ed6b4
Adrian K <goshki@gmail.com>
2011-04-29 15:50:14 +0200

    Disabled -static-link-runtime-shared-libraries option (made texts invisible on Flash Player 10+).

--- build.sh
+++ build.sh
@@ -2,5 +2,7 @@
 
 export WORKSPACE="`pwd`/.."
 
-mxmlc -debug -warnings -strict -optimize -incremental -static-link-runtime-shared-libraries -target-player "9.0.0" -sp=src/main/actionscript src/main/actionscript/pl/vigeo/partisan/PixelPartisan.as && mv src/main/actionscript/pl/vigeo/partisan/PixelPartisan.swf bin/
+#-static-link-runtime-shared-libraries
+
+mxmlc -warnings -strict -optimize -incremental -target-player "9.0.0" -sp=src/main/actionscript src/main/actionscript/pl/vigeo/partisan/PixelPartisan.as && mv src/main/actionscript/pl/vigeo/partisan/PixelPartisan.swf bin/
 
